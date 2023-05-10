7c39995e019f3bc35da0c4e439bf44a3a84746a7
Patricia P <andrograf31@gmail.com>
2023-05-10 11:18:13 +0200

    feat: add test content to window

--- tkinterGUI/main.py
+++ tkinterGUI/main.py
@@ -6,4 +6,10 @@ root = tk.Tk()
 root.geometry("800x500")
 # add title
 root.title("Tkinter practice")
+
+# create content
+label = tk.Label(root, text="Content", font=("Arial",18))
+# add content to window
+label.pack()
+
 root.mainloop()
