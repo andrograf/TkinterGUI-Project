962189b9e8975f80e9a7f55f645bcd765c69a04c
Patricia P <andrograf31@gmail.com>
2023-05-10 11:35:44 +0200

    feat: add textbox to window

--- tkinterGUI/main.py
+++ tkinterGUI/main.py
@@ -10,6 +10,10 @@ root.title("Tkinter practice")
 # create content
 label = tk.Label(root, text="Content", font=("Arial",18))
 # add content to window
-label.pack()
+label.pack(padx=20, pady=20)
+# create a textbox 
+textbox = tk.Text(root, height=3, font=("Arial", 14) )
+# add textbox to window
+textbox.pack(padx=30)
 
 root.mainloop()
