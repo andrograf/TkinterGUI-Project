bc014eaf6dac0040cb6994927c119af02c232f3f
Patricia P <andrograf31@gmail.com>
2023-05-11 11:31:23 +0200

    feat: add label to window

--- tkinterGUI/mainWithClass.py
+++ tkinterGUI/mainWithClass.py
@@ -3,8 +3,13 @@ import tkinter as tk
 class MyGUI:
     # constructor
     def __init__(self):
-        # inicialize window
+        # inicialize a window instant
         self.root = tk.Tk()
+        # create a label(text content)
+        self.label = tk.Label(self.root, text="Content", font=("Arial", 18))
+        # add label to window
+        self.label.pack(padx=20, pady=20)
+        # start mainloop
         self.root.mainloop()
 
 
