1445a4825a26aa6c8df7a3bca31c354b30febd2c
Patricia P <andrograf31@gmail.com>
2023-05-11 11:51:17 +0200

    feat: add button to window

--- tkinterGUI/mainWithClass.py
+++ tkinterGUI/mainWithClass.py
@@ -3,12 +3,14 @@ import tkinter as tk
 class MyGUI:
     # constructor
     def __init__(self):
-        # inicialize a window instant
+        # inicialize a window instant - MUST
         self.root = tk.Tk()
+        
         # create a label(text content)
         self.label = tk.Label(self.root, text="Content", font=("Arial", 18))
         # add label to window
         self.label.pack(padx=20, pady=20)
+        
         # create a textbox 
         self.textbox = tk.Text(self.root, height=5, font=("Arial",16))
         # add textbox to window
@@ -20,7 +22,13 @@ class MyGUI:
         self.checkbox = tk.Checkbutton(self.root, text="checkbox", font=("Arial", 16), variable=self.checkbox_state)
         # add checkbox to window
         self.checkbox.pack()
-        # start mainloop
+        
+        # create button
+        self.message_show_button =  tk.Button(self.root, text="show message", font=("Arial", 14))
+        # add button to window
+        self.message_show_button.pack()
+        
+        # start mainloop - MUST
         self.root.mainloop()
 
 
