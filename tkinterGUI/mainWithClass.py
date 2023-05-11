853bdf34cbf962a6e50b1ad32f219f1d20190c64
Patricia P <andrograf31@gmail.com>
2023-05-11 11:34:28 +0200

    feat: add textbox to window

--- tkinterGUI/mainWithClass.py
+++ tkinterGUI/mainWithClass.py
@@ -9,6 +9,10 @@ class MyGUI:
         self.label = tk.Label(self.root, text="Content", font=("Arial", 18))
         # add label to window
         self.label.pack(padx=20, pady=20)
+        # create a textbox 
+        self.textbox = tk.Text(self.root, height=5, font=("Arial",16))
+        # add textbox to window
+        self.textbox.pack(padx=10, pady=10)
         # start mainloop
         self.root.mainloop()
 
