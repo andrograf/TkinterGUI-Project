d4ade4f0dc518ddbd06cf8a224dd349809a2521f
Patricia P <andrograf31@gmail.com>
2023-05-11 11:23:19 +0200

    feat: create new empty window, using oop & classes

---
+++ tkinterGUI/mainWithClass.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class MyGUI:
+    # constructor
+    def __init__(self):
+        # inicialize window
+        self.root = tk.Tk()
+        self.root.mainloop()
+
+
+
+
+win = MyGUI()
