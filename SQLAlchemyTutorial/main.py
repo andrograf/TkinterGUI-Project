077ff19b975bbc2bc2446af7c3da344f90ddeb5c
Patricia P <andrograf31@gmail.com>
2023-05-14 12:39:21 +0200

    feat: create Person model for person table

--- SQLAlchemyTutorial/main.py
+++ SQLAlchemyTutorial/main.py
@@ -2,3 +2,15 @@ from sqlalchemy import create_engine, ForeignKey, Column, String, CHAR, Integer
 from sqlalchemy.ext.declarative import declarative_base # base class of database
 from sqlalchemy.orm import sessionmaker # from Object-Relational Mapping import Sessionmaker, which create a session
 
+Base = declarative_base()
+
+class Person(Base): # inherit from Base class
+    # add name to database
+    __tablename__ = "people"
+    social_security_number = Column("ssn", Integer,primary_key=True)
+    first_name = Column("firstname", String)
+    last_name = Column("lastname", String)
+    gender = Column("gender", CHAR)
+    age = Column("age", Integer)
+
+    
