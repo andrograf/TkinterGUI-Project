74e0d5780b1a702e0d1c6ea72fbc630d0b701a88
Patricia P <andrograf31@gmail.com>
2023-05-14 12:30:44 +0200

    chore: add imports to main

---
+++ SQLAlchemyTutorial/main.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, CHAR, Integer
+from sqlalchemy.ext.declarative import declarative_base # base class of database
+from sqlalchemy.orm import sessionmaker # from Object-Relational Mapping import Sessionmaker, which create a session
+
